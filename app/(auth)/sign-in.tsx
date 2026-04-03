acb360470985210952e494477c8967aced15b03f
Nbruchi <nkundabagenzibruce@gmail.com>
2026-04-03 14:57:21 +0200

    feat: Add system fonts to the app

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,19 @@
 import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SignIn = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text>SignIn</Text>
       <Link href="/sign-up">Create an account</Link>
-    </View>
+      <Link href="/" className="mt-4 rounded bg-primary p-4 text-white">
+        Go home
+      </Link>
+    </SafeAreaView>
   );
 };
 
