9ba2565dfa7cb8608489807541fd60011b051c59
Nbruchi <nkundabagenzibruce@gmail.com>
2026-04-16 16:22:05 +0200

    chore: add @clerk/expo and expo-secure-store dependencies

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,13 +1,19 @@
+import { useClerk } from "@clerk/expo";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 const Settings = () => {
+  const { signOut } = useClerk();
+
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
       <Text>Settings</Text>
+      <Pressable className="auth-button mt-5" onPress={() => signOut()}>
+        <Text className="auth-button-text">Sign Out</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
